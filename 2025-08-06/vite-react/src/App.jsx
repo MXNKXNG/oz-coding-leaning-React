1ea94aa3a3696e519d244d7199daffb6e7db8d76
min <oldscratch.co@gmail.com>
2025-08-06 15:23:42 +0900

    2일차, React 컴포넌트 만들고, 사용해보기

--- 2025-08-06/vite-react/src/App.jsx
+++ 2025-08-06/vite-react/src/App.jsx
@@ -8,19 +8,83 @@ import './App.css'
 // }
 
 function App() {
-  const number = 10;
   return (
     <>
-      <div>
-        함수형 컴포넌트
-        <div>{ 1 + number }</div>
-      </div>
-      <div>
-        형제 요소
-        <span>자식 요소</span>
+      <빵 />
+      <치즈 />
+      <패티 />
+      <치즈 />
+      <패티 />
+      <토마토 />
+      <양상추 />
+      <빵 />
+    </>
+  )
+}
+
+
+function 빵 () {
+  return (
+    <>
+      <div className="빵">
+        밀가루
+        <spam> 물</spam>
+        <spam> 계란</spam>
+        <spam> 이스트</spam>
       </div>
     </>
   )
 }
+
+function 패티 () {
+  return (
+    <>
+      <div className="패티">
+        소고기
+        <spam> 후추</spam>
+        <spam> 소금</spam>
+      </div>
+    </>
+  )
+}
+
+
+function 양상추 () {
+  return (
+    <>
+      <div className="양상추">
+        잎사귀
+        <spam> 줄기</spam>
+      </div>
+    </>
+  )
+}
+
+function 토마토 () {
+  return (
+    <>
+      <div className="토마토">
+        껍질
+        <spam> 과육</spam>
+        <spam> 씨앗</spam>
+      </div>
+    </>
+  )
+}
+function 치즈 () {
+  return (
+    <>
+      <div className="치즈">
+        우유
+        <spam> 소금</spam>
+        <spam> 색소</spam>
+      </div>
+    </>
+  )
+}
+
+
+
+
 
 export default App
