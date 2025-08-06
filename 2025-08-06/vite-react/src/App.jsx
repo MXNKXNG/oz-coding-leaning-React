9dacfc296a263fbfd3f6df8c9698d920f4778952
min <oldscratch.co@gmail.com>
2025-08-06 18:23:59 +0900

    React 2일차, 과제 실습까지 완료

--- 2025-08-06/vite-react/src/App.jsx
+++ 2025-08-06/vite-react/src/App.jsx
@@ -1,11 +1,73 @@
-// import { Component } from 'react'
+import { Component } from 'react'
 import './App.css'
 
+
+
+// // 과제 1
 // class App extends Component {
-//   render() {
-//     return <div>안녕하세요?</div>
-//   }
+//     render() {
+//         return (
+//             <>
+//                 <h1>React 컴포넌트에는 두 가지 종류가 있죠.</h1>
+//                 <div>
+//                     이것은 클래스형 컴포넌트입니다.
+//                     <p>React 너무나도 재밌죠 ???</p>
+//                     <p>JavaScript와는 다른 새로운 세계가 펼쳐집니다</p>
+//                     <span>기 대 되 는 건 가 ~</span>
+//                 </div>
+//             </>
+//         )
+//     }
 // }
+
+// // 과제 2
+
+// function App () {
+//   return (
+//     <>
+//       <h1>이건 함수형 컴포넌트입니다.</h1>
+//       <p>JaveScript의 함수 문법을 활용하지요!</p>
+//     </>
+//   )
+// }
+
+
+
+
+
+
+// function App() {
+//   const fruits = [
+//     { name: "딸기", color: "빨강", 크기: 2 },
+//     { name: "바나나", color: "노랑", 크기: 5 },
+//     { name: "키위", color: "초록", 크기: 3 },
+//     { name: "사과", color: "빨강", 크기: 4 },
+//     { name: "블루베리", color: "파랑", 크기: 1 },
+//     { name: "오렌지", color: "주황", 크기: 4 },
+//     { name: "포도", color: "보라", 크기: 2 },
+//     { name: "레몬", color: "노랑", 크기: 3 },
+//     { name: "체리", color: "빨강", 크기: 2 },
+//     { name: "수박", color: "초록", 크기: 8 },
+//   ];
+  
+//   return (
+//     <>
+//       {fruits.filter((fruit) => fruit.color === "빨강")
+//         .map((fruit) => (
+//           <div key={fruit.name}>
+//           이름: {fruit.name},
+//           색깔: {fruit.color},
+//           크기: {fruit.크기}
+//           </div>
+//         ))
+//       }
+//     </>
+//   )
+// }
+
+
+
+// ----------------------------
 
 function App() {
   return (
@@ -82,8 +144,6 @@ function 치즈 () {
     </>
   )
 }
-
-
 
 
 
